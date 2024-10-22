358fcdc74f7074563e315134f79f517ee4666b3a
ryananimo64 <ryananimo64@gmail.com>
2024-10-22 17:28:29 -0300

    now the last ones

---
+++ Documents/FRONT END/soma2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n1 ;
+    int n2 ;
+    int rs ;
+    printf("Digite Um número: \n");
+
+    scanf("%d",&n1);
+    printf("Digite outro número:\n");
+    scanf("%d",&n2);
+
+    rs=n1+n2;
+
+    printf("O resultado é %d\n",rs);
+}
