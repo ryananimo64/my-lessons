d94c4fd5a9ca303e4d7515c69864257876e51212
ryananimo64 <ryananimo64@gmail.com>
2024-10-22 17:09:09 -0300

    Today I started playing with the c language

---
+++ Documents/FRONT END/boasvindas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+   printf("-------------- Programa de Mensagem ----------------");
+   printf("\nSeja Bem Vindo ao Nosso Programa\n");
+   printf("----------------- Fim ------------------------------\n");
+
+return 0;
+
+
+}
